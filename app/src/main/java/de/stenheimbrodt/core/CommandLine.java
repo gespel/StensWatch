dd92ba264e6301615892e894b0adb2aa899ca90c
gespel <sten.heimbrodt@hotmail.de>
2024-04-12 11:48:31 +0200

    added cmd stub

---
+++ app/src/main/java/de/stenheimbrodt/core/CommandLine.java
@@ -0,0 +1,12 @@
+package de.stenheimbrodt.core;
+
+public class CommandLine {
+    public CommandLine() {
+
+    }
+
+    void interpret(String input) {
+        String[] split = input.split(" ");
+
+    }
+}
