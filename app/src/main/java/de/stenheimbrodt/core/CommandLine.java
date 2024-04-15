263f552b1b9aa62821b0a085f205649ea6f025d0
gespel <sten.heimbrodt@hotmail.de>
2024-04-15 15:55:49 +0200

    feierabend

--- app/src/main/java/de/stenheimbrodt/core/CommandLine.java
+++ app/src/main/java/de/stenheimbrodt/core/CommandLine.java
@@ -9,12 +9,18 @@ public class CommandLine {
 
     public String interpret(String input) {
         String[] split = input.split(" ");
-        String basecmd = split[0];
-        String[] result = Arrays.copyOfRange(split, 1, split.length);
         String out = "";
 
-        if(basecmd.equalsIgnoreCase("info")) {
-            out = "Made by Sten (Gespel) Heimbrodt";
+        if(split.length >= 1) {
+            String basecmd = split[0];
+            if(split.length >= 2) {
+                String[] result = Arrays.copyOfRange(split, 1, split.length-1);
+            }
+
+
+            if(basecmd.equalsIgnoreCase("info")) {
+                out = "Made by Sten (Gespel) Heimbrodt";
+            }
         }
 
         return out;
